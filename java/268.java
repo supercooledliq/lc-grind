37800fce1dc2a14b8c3f0d595c55a1504903db67
Tanmayee <tanmayeebahadursha@gmail.com>
2025-06-22 17:41:56 +0530

    easypeasy

---
+++ java/268.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+    int totalSum=(nums.length*(nums.length+1))/2;
+    int sum=0;
+    for(int i=0;i<nums.length;i++)
+    {
+        sum+=nums[i];
+    }
+    return totalSum-sum;
+}
+}
+
